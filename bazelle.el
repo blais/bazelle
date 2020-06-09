84790b97ea579d8dc7a3161a1e50a75edb6e0c87
Martin Blais <blais@furius.ca>
2020-06-09 18:05:48 -0400

    Removing bazelle-is-workspace, depending on @emacs-build-mode//lisp:bazel_util.el

--- bazelle.el
+++ bazelle.el
@@ -4,6 +4,8 @@
 ;; Author: Martin Blais <blais@furius.ca>
 ;; Copyright: Apache
 
+(require 'bazel-util)
+
 (defvar bazelle-command "bazel"
   "Bazel executable to run for subprocesses.")
 
@@ -11,17 +13,6 @@
   "Strips the whitespace at the end of string STR."
   (string-match "[ \t\n]*\\'" str)
   (substring str 0 (match-beginning 0)))
-
-(defun bazelle-is-workspace (&optional dirname)
-  "Predicate for whether the given (or current buffer) dirname
-is within a bazel workspace. Returns the workspace root or nil."
-  (let* ((startdir (or dirname
-                       (buffer-file-name)
-                       (and dired-directory (directory-file-name dired-directory))))
-         (rootdir (or (locate-dominating-file startdir "WORKSPACE")
-                      (locate-dominating-file startdir "WORKSPACE.bazel"))))
-    (when rootdir
-      (directory-file-name rootdir))))
 
 (defun bazelle-call-process (&rest args)
   "Run a bazel subcommand. Return stdout as string."
@@ -45,17 +36,19 @@ is within a bazel workspace. Returns the workspace root or nil."
   "Get the list of targets which includes the given filename."
   (let* ((default-directory (file-name-directory filename))
          ;; Resolve label for file with bazel query.
-         (fullname (bazelle-call-process
-                    "query" (file-name-nondirectory filename))))
+         (fullname (bazelle-call-process "query" "--noblock_for_lock"
+                                         (file-name-nondirectory filename))))
     ;; Produce target the file-label is in using bazel query.
     (let ((cmd (format "attr('srcs', %s, %s:*)" fullname
                        (car (split-string fullname ":")))))
-      (split-string (bazelle-call-process "query" cmd)))))
+      (split-string (bazelle-call-process "query" "--noblock_for_lock" cmd)))))
 
 (defun bazelle-build-target-for-directory (dirname)
   "Get the list of targets under the given directory name."
   (let* ((default-directory dirname)
-         (results (split-string (bazelle-call-process "query" "kind('.*rule', ':*')")))
+         (results (split-string
+                   (bazelle-call-process "query" "--noblock_for_lock"
+                                         "kind('.*rule', ':*')")))
          (package (car (split-string (car results) ":"))))
     (append (list (concat package ":all")) results)))
 
@@ -67,30 +60,32 @@ is within a bazel workspace. Returns the workspace root or nil."
 
 (defun bazelle-read-target (&optional filename)
   "Read a target name for the given or current file or dired directory name."
+  ;; Bazel query invocation can be slow, issue a message.
+  (message "Generating completions...")
   (let* ((targets (bazelle-build-target-for-directory-or-filename
                    (or
                     ;; A given filename.
                     filename
-                    ;; The buffer filename.
-                    (buffer-file-name)
                     ;; Open on a BUILD file.
                     (let* ((file-name (buffer-file-name)))
                       (when (and file-name
                                  (string= (file-name-nondirectory file-name) "BUILD"))
                         (directory-file-name (file-name-directory file-name))))
+                    ;; The buffer filename.
+                    (buffer-file-name)
                     ;; Open on a dired directory.
                     (and dired-directory (directory-file-name dired-directory))))))
+    (message (format "TARGETS %s" targets))
     (completing-read "Target: " targets nil nil (car targets))))
 
 (defun bazelle-command-on-current (command)
   "Launch an interactive compilation on the target of the current buffer."
-  (condition-case err
-      (let* ((compile-command
-              (format "cd %s && %s %s %s"
-                      (or (bazelle-is-workspace) (error "Could not find workspace."))
-                      bazelle-command command (bazelle-read-target))))
-        (add-to-list 'compile-history compile-command)
-        (compile compile-command))))
+  (let ((current (or (buffer-file-name)
+                     (and dired-directory (directory-file-name dired-directory)))))
+    (compile (format "cd %s && %s %s %s"
+                     (or (bazel-util-workspace-root current)
+                         (error "Could not find workspace."))
+                   bazelle-command command (bazelle-read-target)))))
 
 (defun bazelle-build ()
   "Build the target for the current buffer."
